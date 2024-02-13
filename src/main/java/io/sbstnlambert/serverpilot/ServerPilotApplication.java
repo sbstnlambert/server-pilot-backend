fb8173e7bb90033ac75f2af7fafbf97c561f1c05
sbstnlambert <sbstn.lambert@gmail.com>
2024-02-13 10:59:39 +0100

    initialized project

---
+++ src/main/java/io/sbstnlambert/serverpilot/ServerPilotApplication.java
@@ -0,0 +1,13 @@
+package io.sbstnlambert.serverpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sbstnlambert/serverpilot/ServerPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sbstnlambert.serverpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
